4f7cf9544af236a2af3143313436149feda431cc
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-05 08:58:20 -0700

    Update Main.php

--- src/BoxOfDevs/BPad/Main.php
+++ src/BoxOfDevs/BPad/Main.php
@@ -34,7 +34,7 @@ class Main extends PluginBase implements Listener {
 			$dx = $direction->getX();
 			$dz = $direction->getZ();
 			if ($this->getConfig()->get("Particle") == "true") {
-				$world->addParticle(new FlameParticle(), $player);
+				$world->addParticle(new FlameParticle(), $player->getPosition());
                                 $world->addParticle(new FlameParticle(), new Vector3($x-0.3, $y, $z));
                                 $world->addParticle(new FlameParticle(), new Vector3($x, $y, $z-0.3));
                                 $world->addParticle(new FlameParticle(), new Vector3($x+0.3, $y, $z));
