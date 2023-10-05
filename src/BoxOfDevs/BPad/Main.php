cdb48e0f9d6e3e08448eed231597a0c836e478eb
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-05 09:34:18 -0700

    Update Main.php

--- src/BoxOfDevs/BPad/Main.php
+++ src/BoxOfDevs/BPad/Main.php
@@ -33,14 +33,17 @@ class Main extends PluginBase implements Listener {
 		$direction = $player->getDirectionVector();
 		$dx = $direction->getX();
 		$dz = $direction->getZ();
-		if($this->config->get("Particle") == "true"){
-	        $world->addParticle(new FlameParticle($player));
-		$world->addParticle(new FlameParticle(), new Vector3($x-0.3, $y, $z));
-		$world->addParticle(new FlameParticle(), new Vector3($x, $y, $z-0.3));
-		$world->addParticle(new FlameParticle(), new Vector3($x+0.3, $y, $z));
-		$world->addParticle(new FlameParticle(), new Vector3($x, $y, $z+0.3));
+
+$boostPower = $this->getConfig()->get('BoostPower');
 		
-		$player->knockBack($dx, $dz, $this->getConfig()->get('BoostPower'));
+$flameParticle = new FlameParticle();
+		
+$world->addParticle($flameParticle, new Vector3($x - 0.3, $y, $z));
+$world->addParticle($flameParticle, new Vector3($x, $y, $z - 0.3));
+$world->addParticle($flameParticle, new Vector3($x + 0.3, $y, $z));
+$world->addParticle($flameParticle, new Vector3($x, $y, $z + 0.3));
+		
+$player->knockBack($dx, $dz, $boostPower);
 	}
     }
 }
