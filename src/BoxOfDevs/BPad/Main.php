885e1772aa5a47ed86b470580d0d114d63822c92
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-05 07:45:16 -0700

    Update Main.php

--- src/BoxOfDevs/BPad/Main.php
+++ src/BoxOfDevs/BPad/Main.php
@@ -4,13 +4,13 @@ namespace BoxOfDevs\BPad;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\level\Level;
-use pocketmine\level\Position;
-use pocketmine\level\particle\Particle;
-use pocketmine\level\particle\FlameParticle;
+use pocketmine\world\World;
+use pocketmine\world\Position;
+use pocketmine\world\particle\Particle;
+use pocketmine\world\particle\FlameParticle;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Server;
 use pocketmine\math\Vector3;
@@ -18,11 +18,10 @@ use pocketmine\block\Block;
 
 class Main extends PluginBase implements Listener {
 	
-	public function onEnable(){
+	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this ,$this);
 		$this->saveDefaultConfig();
 		$this->config = $this->getConfig();
-		$this->getLogger()->info(C::GREEN."Enabled!");
 	}
 	
 	public function onMove(PlayerMoveEvent $event){
@@ -30,24 +29,21 @@ class Main extends PluginBase implements Listener {
 		$x = $player->getX();
 		$y = $player->getY();
 		$z = $player->getZ();
-		$level = $player->getLevel();
-		$block = $level->getBlock($player->getSide(0));
+		$world = $player->getWorld();
+		$block = $world->getBlock($player->getSide(0));
 		if($block->getID() == $this->config->get('Block')){
 			$direction = $player->getDirectionVector();
 			$dx = $direction->getX();
 			$dz = $direction->getZ();
 			if($this->config->get("Particle") == "true"){
-				$level->addParticle(new FlameParticle($player));
-				$level->addParticle(new FlameParticle(new Vector3($x-0.3, $y, $z)));
-				$level->addParticle(new FlameParticle(new Vector3($x, $y, $z-0.3)));
-				$level->addParticle(new FlameParticle(new Vector3($x+0.3, $y, $z)));
-				$level->addParticle(new FlameParticle(new Vector3($x, $y, $z+0.3)));
+				$world->addParticle(new FlameParticle($player));
+				$world->addParticle(new FlameParticle(new Vector3($x-0.3, $y, $z)));
+				$world->addParticle(new FlameParticle(new Vector3($x, $y, $z-0.3)));
+				$world->addParticle(new FlameParticle(new Vector3($x+0.3, $y, $z)));
+				$world->addParticle(new FlameParticle(new Vector3($x, $y, $z+0.3)));
 			}
 			$player->knockBack($player, 0, $dx, $dz, $this->config->get('BoostPower'));
 		}
 	}
 	
-	public function onDisable(){
-		$this->getLogger()->info(C::DARK_RED."Disabled!");
-	}
 }
